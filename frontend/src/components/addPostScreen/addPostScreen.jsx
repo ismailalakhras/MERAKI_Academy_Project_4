8ee1cef11a81631a6ece45d1c8dca4878bed2dfb
ismailalakhras <ismail.malakhras@gmail.com>
2024-06-11 20:25:15 +0300

    add post function

--- frontend/src/components/addPostScreen/addPostScreen.jsx
+++ frontend/src/components/addPostScreen/addPostScreen.jsx
@@ -96,12 +96,26 @@ import "./addPostScreen.css";
 // };
 
 const AddPostScreen = ({ setAddPostScreen }) => {
+  const inputRef = useRef(null);
+
+  const [file, setFile] = useState("");
+  const [image, setImage] = useState("");
+  const [postContent, setPostContent] = useState("");
+
+  const { token, toggle, setToggle } = useContext(AppContext);
   return (
     <div className="screenPage">
       <div className="addPost_container">
         <div className="addPost_container-top">
           <div className="title">create post</div>
-          <div className="x">X</div>
+          <div
+            onClick={() => {
+              setAddPostScreen(false);
+            }}
+            className="x"
+          >
+            X
+          </div>
         </div>
         <div className="addPost_container-user">
           <div className="image">
@@ -110,14 +124,72 @@ const AddPostScreen = ({ setAddPostScreen }) => {
           <div className="userName">Xxxxxx</div>
         </div>
         <div className="addPost_container-textArea">
-          <textarea placeholder="whats on your mind" name="Whats on your mind" id=""></textarea>
+          <textarea
+            value={postContent}
+            onChange={(e) => {
+              setPostContent(e.target.value);
+            }}
+            
+            placeholder="whats on your mind"
+            name="Whats on your mind"
+        
+          ></textarea>
         </div>
-        <div className="addPost_container-image">
-          <img src={require("../../pic/logo.png")} alt="" />
+        <div
+          onClick={() => {
+            inputRef.current.click();
+          }}
+          className="addPost_container-image"
+        >
+          {image ? (
+            <img src={URL.createObjectURL(image)} alt="" />
+          ) : (
+            <img className="no-img" src={require("../../pic/no-img.png")} alt="" />
+          )}
+
+          <input
+            ref={inputRef}
+            type="file"
+            onChange={(e) => {
+              setFile(e.target.files[0]);
+              setImage(e.target.files[0]);
+            }}
+          />
+
+          {/* <img src={require("../../pic/logo.png")} alt="" /> */}
         </div>
 
         <div className="addPost_container-button">
-          <div>Post</div>
+          <div
+            onClick={() => {
+              const formData = new FormData();
+
+              formData.append("image", file);
+              formData.append("postContent", postContent);
+              axios
+                .post(
+                  "http://localhost:5000/createPost/upload",
+                  formData,
+
+                  {
+                    headers: {
+                      Authorization: `Bearer ${token}`,
+                    },
+                  }
+                )
+                .then((result) => {
+                  console.log(result.data.post);
+
+                  setToggle(!toggle);
+                  setAddPostScreen(false);
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            }}
+          >
+            Post
+          </div>
         </div>
       </div>
     </div>
