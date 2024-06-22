e034ac3fb777b5eda8d11d9c5eecf465bcbbbf70
ismailalakhras <ismail.malakhras@gmail.com>
2024-06-22 23:01:08 +0300

    socket chat

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 
-const http = require("http")
-const Server = require('socket.io').Server
 
 const cors = require("cors");
 
@@ -17,38 +15,6 @@ const messageRouter = require("./routes/message");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-//! -----------------------------------------------------
-//! -----------------------------------------------------
-//! -----------------------------------------------------
-
-const server = http.createServer(app)
-
-const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
-})
-
-io.on("connection", (socket) => {
-  console.log("ew are connected");
-
-  socket.on("chat", (chat) => {
-    io.emit("chat", chat)
-  })
-
-  io.on("disconnect", () => {
-    console.log("disconnected");
-  })
-})
-
-
-
-
-
-//! -----------------------------------------------------
-//! -----------------------------------------------------
-//! -----------------------------------------------------
 
 
 app.use(cors());
@@ -68,6 +34,6 @@ app.use('/message', messageRouter)
 // Handles any other endpoints [unassigned - endpoints]
 app.use("*", (req, res) => res.status(404).json("No content at this path"));
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
